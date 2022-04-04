dba6af3cd4ed062591ead4afe79db9b3f6b0d351
SumiAkter2 <akteralisha36@gmail.com>
2022-04-04 17:58:38 +0600

    added about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,17 @@
 import React from 'react';
-
+import {CartPieIcon, CashIcon, GiftIcon, MenuIcon, StarIcon, ThumbDownIcon, TrendingDownIcon, TrendingUpIcon } from '@heroicons/react/solid';
+import './About.css'
 const About = () => {
     return (
         <div>
-            <h1>About</h1>
+            <h1 style={{marginBottom:'100px',marginTop:'80px', color:"darkblue"}}>Best Features</h1>
+            <div className='about-container'>
+                <h1><CashIcon className='icons'></CashIcon> Multi Currency Support </h1>
+                <h1><MenuIcon className='icons'></MenuIcon> Multi Menu Support </h1>
+                <h1><GiftIcon className='icons'></GiftIcon>Gift Voucher </h1>
+                <h1><TrendingUpIcon className='icons'></TrendingUpIcon>Business policy </h1>
+               
+            </div>
         </div>
     );
 };
